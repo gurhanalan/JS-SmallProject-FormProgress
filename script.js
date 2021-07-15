777295221c13c379e9d054aa9829f774bcef4596
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-15 19:30:14 +0300

    Add waveform js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const labels = document.querySelectorAll(".input-box label");
 
+function addSpans() {
+    labels.forEach((label) => {
+        label.innerHTML = label.innerText
+            .split("")
+            .map(
+                (letter, index) =>
+                    `<span style="transition-delay:${
+                        index * 50
+                    }ms;">${letter}</span>`
+            )
+            .join("");
+    });
+}
+
+addSpans();
