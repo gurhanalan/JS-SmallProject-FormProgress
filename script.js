bae380859e21101bebcd42bcc3e943022cdc03ad
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-15 20:01:22 +0300

    Add progress bar

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ function addSpans() {
 }
 
 addSpans();
+
+// Progress Bars
+// Selections
+
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+
+let step = 0;
+
+function btnDisable() {
+    if (step >= 3) {
+        btnNext.disabled = true;
+    } else if (step <= 0) {
+        btnPrev.disabled = true;
+    } else {
+        btnNext.disabled = false;
+        btnPrev.disabled = false;
+    }
+}
+
+btnNext.addEventListener("click", () => {
+    step++;
+    document.querySelector(`.bridge-${step}`).classList.add("bridge--active");
+    document.querySelector(`.step-${step}`).classList.add("step--active");
+    btnDisable();
+});
+
+btnPrev.addEventListener("click", () => {
+    document
+        .querySelector(`.bridge-${step}`)
+        .classList.remove("bridge--active");
+    document.querySelector(`.step-${step}`).classList.remove("step--active");
+    step--;
+    btnDisable();
+});
+btnDisable();
